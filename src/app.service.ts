37b58b49d1c11e59272142b58499b1f8ee5458f4
ab-hakim-ayman <133145031+ab-hakim-ayman@users.noreply.github.com>
2024-09-28 03:23:53 +0600

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello DevOps World!';
+    return 'Hello DevOps bbb World!';
   }
 }
